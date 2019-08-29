3c92113e1a38179aa7353433fae7789fc0974aca
Mohamed Abou-Dabous <mohamed.abou-dabous@chasacademy.se>
2019-08-29 14:35:41 +0200

    more deployment fixes for gods sake heroku

--- recipe-app/server.js
+++ recipe-app/server.js
@@ -5,11 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/06-recipe-app-frontend-mohamedaboudabous'));
+app.use(
+  express.static(__dirname + '/06-recipe-app-frontend-mohamedaboudabous')
+);
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/06-recipe-app-frontend-mohamedaboudabous'));
+app.get('/*', function(req, res) {
+  res.sendFile(
+    path.join(
+      __dirname + '/dist/06-recipe-app-frontend-mohamedaboudabous/index.html'
+    )
+  );
 });
 
 // Start the app by listening on the default Heroku port
